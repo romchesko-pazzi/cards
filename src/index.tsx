5533270d88430070f9fb16f95eaff01a528ed0ec
romchesko-pazzi <romchesko.pazzi@gmail.com>
2022-09-28 22:20:51 +0300

    fix: 15/19 lines - fix bug with useEffect

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import './assets/sass/_main.scss';
 
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
-import { App } from './App';
+import { App } from './app/App';
 import reportWebVitals from './reportWebVitals';
+import { store } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </Provider>,
 );
 reportWebVitals();
