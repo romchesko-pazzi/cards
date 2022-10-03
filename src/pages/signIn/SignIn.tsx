754a78179c674314607d98b811b2f740e85e345a
romchesko-pazzi <romchesko.pazzi@gmail.com>
2022-10-03 23:20:58 +0300

    feat: add a little more email validation

--- src/pages/signIn/SignIn.tsx
+++ src/pages/signIn/SignIn.tsx
@@ -5,11 +5,12 @@ import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 
 import { ButtonComponent } from '../../components/button/ButtonComponent';
+import { SnackBar } from '../../components/snackBar/SnackBar';
 import { login } from '../../store/reducers/AuthReducer';
 import { useAppDispatch, useAppSelector } from '../../utils/hooks/hooks';
-import { LoginDataType } from '../../utils/types/types';
+import { SignInType } from '../../utils/types/types';
 
-import s from './SignIn.module.scss';
+import s from './signIn.module.scss';
 
 export const SignIn = () => {
   const { isLoggedIn } = useAppSelector(state => state.auth);
@@ -20,20 +21,20 @@ export const SignIn = () => {
     if (isLoggedIn) {
       navigate('/profile');
     }
-  }, [isLoggedIn]);
-
+  }, [isLoggedIn, navigate]);
   const {
     handleSubmit,
     reset,
     register,
     formState: { errors },
-  } = useForm<LoginDataType>({
+  } = useForm<SignInType>({
     mode: 'onBlur',
   });
-  const onSubmit = (data: LoginDataType) => {
+  const onSubmit = (data: SignInType) => {
     dispatch(login(data));
     reset();
   };
+  const navigateToSignUp = () => navigate('/signUp');
 
   return (
     <div className={s.main}>
@@ -43,6 +44,10 @@ export const SignIn = () => {
           <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
             <TextField
               {...register('email', {
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: 'Invalid email format',
+                },
                 required: 'Email is required',
               })}
               label={errors.email?.message}
@@ -51,6 +56,7 @@ export const SignIn = () => {
               error={!!errors.email?.message}
               variant="outlined"
               placeholder="Email"
+              type="email"
             />
             <TextField
               {...register('password', {
@@ -62,6 +68,7 @@ export const SignIn = () => {
               error={!!errors.password?.message}
               variant="outlined"
               placeholder="Password"
+              type="password"
             />
             <div className={s.checkbox}>
               <Checkbox
@@ -76,16 +83,21 @@ export const SignIn = () => {
             <div className={s.button}>
               <ButtonComponent type="submit" title="Sign in" />
             </div>
-            <div className={s.forgotArea}>
+            <div className={s.bottomArea}>
               <div style={{ textAlign: 'end', color: '#366EFF' }}>Forgot Password?</div>
               <div style={{ textAlign: 'center' }}>Do not have an account?</div>
               <div className={s.signUp}>
-                <ButtonComponent title="Sign up" />
+                <ButtonComponent
+                  callback={navigateToSignUp}
+                  type="button"
+                  title="Sign up"
+                />
               </div>
             </div>
           </form>
         </div>
       </div>
+      <SnackBar />
     </div>
   );
 };
