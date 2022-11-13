7f77bf9a27ab79ee0f5896a3bf8874111d215f8d
romchesko-pazzi <romchesko.pazzi@gmail.com>
2022-11-13 17:09:30 +0300

    feat/fix: add popover feature + fix bug with cards generation (51 line(CardsList))

--- src/components/svgSelector/SvgSelector.tsx
+++ src/components/svgSelector/SvgSelector.tsx
@@ -373,6 +373,21 @@ export const SvgSelector = memo(({ id }: PropsType) => {
           />
         </svg>
       );
+    case 'settings':
+      return (
+        <svg
+          width="18"
+          height="18"
+          viewBox="0 0 18 18"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle cx="9" cy="9" r="8.5" stroke="#0D0C0B" />
+          <circle cx="9.00049" cy="5.50024" r="0.875" fill="black" />
+          <circle cx="9.00049" cy="8.99976" r="0.875" fill="black" />
+          <circle cx="9.00049" cy="12.5" r="0.875" fill="black" />
+        </svg>
+      );
     default:
       return <svg />;
   }
--- src/pages/cardsList/CardsList.tsx
+++ src/pages/cardsList/CardsList.tsx
@@ -7,8 +7,10 @@ import c from '../../assets/commonStyles/common.module.scss';
 import { Captions } from '../../components/captions/Captions';
 import { AddModal } from '../../components/modals/addModal/AddModal';
 import { PaginationComponent } from '../../components/pagination/PaginationComponent';
+import { PopoverComponent } from '../../components/popover/PopoverComponent';
 import { Search } from '../../components/search/Search';
 import { SnackBar } from '../../components/snackBar/SnackBar';
+import { setIsCardsFetched } from '../../store/reducers/CardsReducer';
 import { getCards } from '../../store/thunks/thunks';
 import { cardsCaptions, path } from '../../utils/constants/constants';
 import { useAppDispatch, useAppSelector } from '../../utils/hooks/useSelectorUseDispatch';
@@ -44,18 +46,24 @@ export const CardsList = () => {
     dispatch(getCards(packId));
     // задаём init state, иначе setSearchParams location.state = null
     setSearchParams({ packId, userId }, { state: { packId, packName, userId } });
+
+    // clear cards in store
+    return () => {
+      dispatch(setIsCardsFetched(false));
+    };
   }, [dispatch, packId, cardQuestion, pageCount, currentPage, sortBy]);
 
   return (
     <div className={c.frame}>
-      <Link to={path.packsList}>
-        <div className={c.returnToPackList}>
-          <ArrowBackIcon fontSize="large" />
-          <span>Back to pack list</span>
-        </div>
+      <Link className={c.returnToPackList} to={path.packsList}>
+        <ArrowBackIcon fontSize="large" />
+        <span>Back to pack list</span>
       </Link>
       <div className={c.heading}>
-        <h3>{packName}</h3>
+        <div className={s.interactWithPack}>
+          <h3>{packName}</h3>
+          <PopoverComponent packName={packName} packId={packId} />
+        </div>
         <AddModal isThisPlaceCards />
       </div>
       <div className={c.settings}>
