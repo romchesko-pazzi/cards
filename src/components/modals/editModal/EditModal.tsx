c10f53cb2a3b1f9751dff13bc56887a5af7b9d5d
romchesko-pazzi <romchesko.pazzi@gmail.com>
2022-11-14 22:36:48 +0300

    feat: add validation on editModal

--- src/components/modals/editModal/EditModal.tsx
+++ src/components/modals/editModal/EditModal.tsx
@@ -1,43 +1,53 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
+import { yupResolver } from '@hookform/resolvers/yup/dist/yup';
 import CloseIcon from '@mui/icons-material/Close';
 import { TextField } from '@mui/material';
+import { useForm } from 'react-hook-form';
 import { useSearchParams } from 'react-router-dom';
 
 import { updateCard, updatePackName } from '../../../store/thunks/thunks';
 import { useAppDispatch } from '../../../utils/hooks/useSelectorUseDispatch';
+import { addModalValidate } from '../../../utils/validators/validators';
 import { ButtonComponent } from '../../button/ButtonComponent';
 import { BaseModal } from '../baseModal/BaseModal';
 import c from '../commonModal.module.scss';
 
 export const EditModal = (props: PropsType) => {
   const { propsUserId, id, name, isThisPlaceCards, optionName } = props;
-  const [firstValue, setFirstValue] = useState<string>(name);
-  const [secondValue, setSecondValue] = useState<string>(optionName || '');
   const [searchParams] = useSearchParams(); // for cardsComponent
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    return () => {
+      reset();
+    };
+  }, [isOpen]);
 
   const userId = searchParams.get('userId') || propsUserId;
   const placeholder = isThisPlaceCards ? 'Question' : 'Pack Name';
 
   const setIsOpenHandler = () => setIsOpen(!isOpen);
+  const {
+    handleSubmit,
+    reset,
+    register,
+    formState: { errors },
+  } = useForm<TextFieldsType>({
+    mode: 'onSubmit',
+    resolver: yupResolver(addModalValidate),
+    defaultValues: {
+      firstValue: name,
+      secondValue: optionName || '',
+    },
+  });
 
-  const onFirstInputChangeHandler = (
-    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
-    setFirstValue(event.currentTarget.value);
-  };
-  const onSecondInputChangeHandler = (
-    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
-    setSecondValue(event.currentTarget.value);
-  };
-
-  const editPackHandler = () => {
-    if (!isThisPlaceCards) dispatch(updatePackName(id, firstValue));
-    if (isThisPlaceCards) dispatch(updateCard(firstValue, secondValue, id));
+  const onSubmit = (data: TextFieldsType) => {
+    if (!isThisPlaceCards) dispatch(updatePackName(id, data.firstValue));
+    if (isThisPlaceCards) dispatch(updateCard(data.firstValue, data.secondValue!, id));
     setIsOpenHandler();
+    reset();
   };
 
   return (
@@ -48,41 +58,47 @@ export const EditModal = (props: PropsType) => {
           <CloseIcon cursor="pointer" fontSize="large" />
         </button>
       </div>
-      <div className={c.input}>
-        <TextField
-          InputProps={{ className: c.textField }}
-          sx={{ minWidth: '100%' }}
-          autoFocus
-          variant="standard"
-          onChange={onFirstInputChangeHandler}
-          value={firstValue}
-          type="text"
-          placeholder={placeholder}
-        />
-      </div>
-      {isThisPlaceCards && (
-        <div className={c.secondInput}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={c.input}>
           <TextField
+            {...register('firstValue')}
+            error={!!errors.firstValue?.message}
+            label={errors.firstValue?.message}
             InputProps={{ className: c.textField }}
+            InputLabelProps={{ className: c.labelText }}
             sx={{ minWidth: '100%' }}
             autoFocus
             variant="standard"
-            onChange={onSecondInputChangeHandler}
-            value={secondValue}
             type="text"
-            placeholder="Answer"
+            placeholder={placeholder}
           />
         </div>
-      )}
-      <div className={c.buttons}>
-        <ButtonComponent
-          disabled={false}
-          title="Cancel"
-          callback={setIsOpenHandler}
-          color="red"
-        />
-        <ButtonComponent disabled={false} title="Save" callback={editPackHandler} />
-      </div>
+        {isThisPlaceCards && (
+          <div className={c.secondInput}>
+            <TextField
+              {...register('secondValue')}
+              error={!!errors.secondValue?.message}
+              label={errors.secondValue?.message}
+              InputProps={{ className: c.textField }}
+              InputLabelProps={{ className: c.labelText }}
+              sx={{ minWidth: '100%' }}
+              autoFocus
+              variant="standard"
+              type="text"
+              placeholder="Answer"
+            />
+          </div>
+        )}
+        <div className={c.buttons}>
+          <ButtonComponent
+            disabled={false}
+            title="Cancel"
+            callback={setIsOpenHandler}
+            color="red"
+          />
+          <ButtonComponent disabled={false} title="Save" type="submit" />
+        </div>
+      </form>
     </BaseModal>
   );
 };
@@ -93,4 +109,9 @@ type PropsType = {
   id: string;
   name: string;
   isThisPlaceCards: boolean;
+};
+
+type TextFieldsType = {
+  firstValue: string;
+  secondValue?: string;
 };
